a7bb881d76e02b5083be04378483b96bf681a405
brian <brian@I9>
2022-09-24 19:55:30 -0400

    Add project files.

---
+++ CalculatePi/Program.cs
@@ -0,0 +1,10 @@
+﻿var factor = 1M;
+decimal fourthOfPI = 0;
+
+for (decimal denominator = 1; denominator < decimal.MaxValue - 2; denominator += 2)
+{
+    fourthOfPI = fourthOfPI + (factor * (1M / denominator));
+    factor = factor * -1;
+
+    Console.WriteLine(fourthOfPI*4);
+}
